8e55d5b3129d80752e400dd6a3bb60d0b4a09c3f
Samuel Emeka <samuel.emeka@kibo.school>
2024-08-31 15:32:11 +0100

    Implemented the interface

--- src/containers/Art.tsx
+++ src/containers/Art.tsx
@@ -1,7 +1,9 @@
 import type { Art as ArtType } from "../constants/arts";
 import "../styles/art.css";
+import { FaRegHeart } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
 import { FaRegComment } from "react-icons/fa";
+import { useState } from "react";
 
 
 
@@ -11,27 +13,35 @@ interface ArtProps {
 }
 
 const Art = ({ art }: ArtProps) => {
+
+  const [ isLiked, setIsLiked] = useState(false)
+
+  const handleLike =() => {
+    setIsLiked(prev => !prev)
+  }
   return (
     <div className="art-container">
       <img className="art-image" src={art.image} alt={art.name} />
       <div className="like-and-comment">
         <div className="likecomment">
           <div>
-            <button className="like-button"><FaHeart className="heart"/></button>
+            <button onClick={handleLike} className="like-button">
+              {isLiked ? <FaHeart className="art-icon"  color="black"/> : <FaRegHeart className="art-icon" />}
+            </button>
           </div>
           <div>
-            <button className="comment-button"><FaRegComment className="comment"/></button>
+            <button className="comment-button"><FaRegComment className="art-icon"/></button>
           </div>
         </div>
 
         <div className="countlikes">
-          <p>{art.numberOfLikes} likes</p>
+          <p className="">{art.numberOfLikes} likes</p>
           <p>{art.comments.length} comments</p>
         </div>
       </div>
-      <h3>{art.name}</h3>
+      <h3 className="art-name">{art.name}</h3>
       <div className="art-details">
-        <p>{art.description}</p>
+        <p className="art-description">{art.description.length > 200 ? art.description.slice(0, 200) + "..." : art.description}</p>
       </div>
     </div>
   );
