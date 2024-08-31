c3b9114555e67c4a28b776e1b6c3879d8ccb2a46
Samuel Emeka <samuel.emeka@kibo.school>
2024-08-31 12:33:25 +0100

    first commit

---
+++ src/api/firebase.ts
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCb4zcJjRputLeD3anbSjIr0Xn5_qPcjUE",
+  authDomain: "artify-b41fe.firebaseapp.com",
+  projectId: "artify-b41fe",
+  storageBucket: "artify-b41fe.appspot.com",
+  messagingSenderId: "400952453429",
+  appId: "1:400952453429:web:5f0e343b5dd50e3f49847c"
+};
+
+
+export const app = initializeApp(firebaseConfig);
