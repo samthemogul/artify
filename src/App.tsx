de66a16e722a7ea6204c1bee0ff3cceb6547b621
Samuel Emeka <samuel.emeka@kibo.school>
2024-09-03 17:06:38 +0100

    chore: Update styles for art component and add new pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
+import ArtDetail from "./pages/ArtDetail";
+import UploadFile from "./pages/UploadFile";
+import ArtistProfile from "./pages/ArtistProfile";
 import './App.css'
 
 function App() {
   
 
   return (
-    <Home />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/arts/:artId" element={<ArtDetail />} />
+      <Route path="/new" element={<UploadFile />} />
+      <Route path="/artists/:artistId" element={<ArtistProfile />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/containers/Art.tsx
+++ src/containers/Art.tsx
@@ -1,11 +1,12 @@
 import type { Art as ArtType } from "../constants/arts";
 import "../styles/art.css";
-import { FaRegHeart } from "react-icons/fa";
-import { FaHeart } from "react-icons/fa6";
-import { FaRegComment } from "react-icons/fa";
-import { useState } from "react";
-
-
+import { IoHeartOutline } from "react-icons/io5";
+import { IoHeartSharp } from "react-icons/io5";
+import { IoChatbubbleOutline } from "react-icons/io5";
+import { HiOutlineDotsVertical } from "react-icons/hi";
+import profilePic from '../assets/profile.png'
+import { SetStateAction, useState } from "react";
+import arts from "../constants/arts";
 
 interface ArtProps {
   key: string;
@@ -13,35 +14,70 @@ interface ArtProps {
 }
 
 const Art = ({ art }: ArtProps) => {
+  const [isLiked, setIsLiked] = useState(false);
+  const [ comment, setComment ] = useState("")
 
-  const [ isLiked, setIsLiked] = useState(false)
+  const handleLike = () => {
+    setIsLiked((prev) => !prev);
+  };
 
-  const handleLike =() => {
-    setIsLiked(prev => !prev)
+  const handleChangeComment = (event: { target: { value: SetStateAction<string>; }; }) => {
+    setComment(event.target.value)
   }
   return (
     <div className="art-container">
+      <div className="art-header">
+        <div className="art-artist">
+          <img className="art-artist-pic" src={profilePic} alt="profile" />
+          <p className="artist-name">{art.artist}</p>
+        </div>
+        <div className="art-menu">
+          <button className="menu-button">
+            <HiOutlineDotsVertical className="art-menu-icon" />
+          </button>
+        </div>
+      </div>
       <img className="art-image" src={art.image} alt={art.name} />
       <div className="like-and-comment">
         <div className="likecomment">
           <div>
             <button onClick={handleLike} className="like-button">
-              {isLiked ? <FaHeart className="art-icon"  color="black"/> : <FaRegHeart className="art-icon" />}
+              {isLiked ? (
+                <IoHeartSharp className="art-icon" color="black" />
+              ) : (
+                <IoHeartOutline className="art-icon" />
+              )}
             </button>
           </div>
           <div>
-            <button className="comment-button"><FaRegComment className="art-icon"/></button>
+            <button className="comment-button">
+              <IoChatbubbleOutline className="art-icon" />
+            </button>
           </div>
         </div>
 
         <div className="countlikes">
           <p className="">{art.numberOfLikes} likes</p>
-          <p>{art.comments.length} comments</p>
         </div>
       </div>
       <h3 className="art-name">{art.name}</h3>
       <div className="art-details">
-        <p className="art-description">{art.description.length > 200 ? art.description.slice(0, 200) + "..." : art.description}</p>
+        <p className="art-description">
+          {art.description.length > 200
+            ? art.description.slice(0, 200) + "..."
+            : art.description}
+        </p>
+      </div>
+      <button className="view-comment-btn">
+        {art.comments.length < 1
+          ? null
+          : art.comments.length == 1
+          ? "View comment"
+          : `View all ${art.comments.length} comments`}
+      </button>
+      <div className="comment-section">
+        <input className="comment-input" value={comment} onChange={handleChangeComment} type="text" placeholder="Add a comment..." />
+        { comment.length > 2 ? <button className="post-comment">Post</button> : null}
       </div>
     </div>
   );
